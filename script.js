141cfb119f82e874924857277ec1643feaadd4ba
garcia-gl <gabriel.garcia.leal@escola.pr.gov.br>
2025-09-12 07:55:49 -0300

    salvar

---
+++ script.js
@@ -0,0 +1,7 @@
+const avanca = document.querySelectorAll('.btm-proximo');
+
+avanca.forEach(buton => {
+    GamepadButton.addEventLitener('click', function(){
+        const atual = document.querySelector('.ativo')
+    })
+})
